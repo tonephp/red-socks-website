1fc15b97bd441411a6eef58a1cdb54e4f3108d49
Andrii Pereverziev <a.pereverziev@mail.enamine.net>
2021-07-10 12:31:11 +0300

    Tutorial. Coming Soon - Create coming-soon component

--- app/pages/Tutorials/pages/coming-soon-create-coming-soon-component.php
+++ app/pages/Tutorials/pages/coming-soon-create-coming-soon-component.php
@@ -1,1 +1,39 @@
 <h1>Create Coming Soon Component</h1>
+
+<h2>1. Create file app/components/coming-soon/coming-soon.php</h2>
+<pre>
+  <code class="language-php">
+<?=doc('/pages/Tutorials/pages/coming-soon-create-coming-soon-component/coming-soon.php')?>
+  </code>
+</pre>
+
+<h2>2. Create file app/components/coming-soon/coming-soon.scss</h2>
+<pre>
+  <code class="language-scss">
+<?=doc('/pages/Tutorials/pages/coming-soon-create-coming-soon-component/coming-soon.scss')?>
+  </code>
+</pre>
+
+<h2>3. Import coming-soon.scss into app/src/scss/styles.scss</h2>
+<pre>
+  <code class="language-scss">
+...
+@import "../components/coming-soon/coming-soon.scss";
+...
+  </code>
+</pre>
+
+<h2>4. Use coming-soon component in app/components/grid/grid.php</h2>
+<p>Paste this code in the block with class "grid__main"</p>
+<pre>
+  <code class="language-php">
+<?=doc('/pages/Tutorials/pages/coming-soon-create-coming-soon-component/grid.php')?>
+  </code>
+</pre>
+<p>grid.php should looks like on the picture:</p>
+<img src="/img/tutorials/coming-soon-create-coming-soon-component-grid.png" alt="coming-soon-create-coming-soon-component-grid" class="doc__image" />
+
+<h2>5. See result</h2>
+<p>Open this link - <a href="http://localhost:40001" target="_blank">http://localhost:40001</a></p>
+<p>You should see the site like on the picture:</p>
+<img src="/img/tutorials/coming-soon-create-coming-soon-component-final.jpg" alt="coming-soon-create-coming-soon-component-final" class="doc__image" />
---
+++ app/pages/Tutorials/pages/coming-soon-create-coming-soon-component/coming-soon.php
@@ -0,0 +1,10 @@
+<div class="coming-soon">
+  <h1>Coming soon</h1>
+  <p>
+    Our website is currently under construction. We Should be back shortly. Thank you for your patience.
+  </p>
+  FORM
+  <p>
+    Sign up now to get early notification of our launch date!
+  </p>
+</div>
